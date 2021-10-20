6a6b3b1d5004c0b6301d2387d49934c11611b0d2
Johan Legarda <legardajohan@gmail.com>
2021-10-20 16:20:13 -0500

    Configurando Composer

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Llamando a una funcion para MAYUSCULAS
+
+echo upper('hola mundo!');
+
+// Llamando a una funcion para minusculas
+
+echo '<br>' . minusculas('HOLA MUNDO!');
+
+
+?>
---
+++ src/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function upper ($value) {
+    return Text\Format::upperText($value);
+}
+
+function minusculas($value) {
+    return Text\Format::lowerText($value);
+}
+
+?>
